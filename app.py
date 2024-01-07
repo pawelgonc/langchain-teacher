74c6d0a30bc471e70c2842182c9bb9169306bc81
pawelgonc <pawel.gonc@gmail.com>
2024-01-07 17:10:26 +0100

    separate sessions for teacher and creator

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ PAGES = {
 }
 
 def main():
+    # Initialize the session state for 'Teacher' and 'Lesson Creator'
+    if "teacher" not in st.session_state:
+        st.session_state.teacher = {}
+    if "lesson_creator" not in st.session_state:
+        st.session_state.creator = {}
+
     st.sidebar.title('Navigation')
     selection = st.sidebar.radio("Go to", list(PAGES.keys()))
     page = PAGES[selection]
