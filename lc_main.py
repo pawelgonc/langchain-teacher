0946e396195bc7c8cecf78a2685aa9e3f91acaf3
pawelgonc <pawel.gonc@gmail.com>
2024-01-07 14:32:50 +0100

    renaming variables for readability

--- lc_main.py
+++ lc_main.py
@@ -36,29 +36,29 @@ class StreamHandler(BaseCallbackHandler):
 class Lesson:
     def __init__(self, filename):
         self.filename = filename
-        self.sections = ["title", "background_and_prerequisites", "learning_objectives", "content_delivery", "introduction", "main_points", "conclusion", "next_steps"]
-        self.current_section_index = 0
+        self.lesson_sections = ["title", "background_and_prerequisites", "learning_objectives", "content_delivery", "introduction", "main_points", "conclusion", "next_steps"]
+        self.active_section_index = 0
         self.load_content()
 
     def update_current_section(self):
-        if self.current_section_index < len(self.sections):
-            self.current_section = self.sections[self.current_section_index]
-            self.current_section_index += 1
+        if self.active_section_index < len(self.lesson_sections):
+            self.active_section = self.lesson_sections[self.active_section_index]
+            self.active_section_index += 1
         else:
-            self.current_section = None  # No more sections
+            self.active_section = None  # No more lesson_sections
 
     def load_content(self):
         with open(f"lessons/{self.filename}", "r") as file:
             content = file.read()
-            sections = content.split("-----")
-            self.title = sections[1].strip()
-            self.background_and_prerequisites = sections[2].strip()
-            self.learning_objectives = sections[3].strip()
-            self.content_delivery = sections[4].strip()
-            self.introduction = sections[5].strip()
-            self.main_points = self.parse_main_points(sections[6])
-            self.conclusion = sections[7].strip()
-            self.next_steps = sections[8].strip()
+            lesson_sections = content.split("-----")
+            self.title = lesson_sections[1].strip()
+            self.background_and_prerequisites = lesson_sections[2].strip()
+            self.learning_objectives = lesson_sections[3].strip()
+            self.content_delivery = lesson_sections[4].strip()
+            self.introduction = lesson_sections[5].strip()
+            self.main_points = self.parse_main_points(lesson_sections[6])
+            self.conclusion = lesson_sections[7].strip()
+            self.next_steps = lesson_sections[8].strip()
 
     def parse_main_points(self, main_points_section):
         main_points = main_points_section.split("\n-----")
@@ -70,29 +70,29 @@ class Lesson:
         st.write(self.learning_objectives)
 
 def handle_user_input():
-    if prompt := st.chat_input():
-        st.chat_message("user").write(prompt)
-        return prompt
+    if user_input := st.chat_input():
+        st.chat_message("user").write(user_input)
+        return user_input
     return None
 
-def handle_assistant_response(prompt, lesson):
-    lesson.update_current_section()  # Update current_section before using it
+def handle_assistant_response(user_input, current_lesson):
+    current_lesson.update_current_section()  # Update current_section before using it
 
     with st.chat_message("assistant"):
         stream_handler = StreamHandler(st.empty())
         model = ChatOpenAI(streaming=True, callbacks=[stream_handler], model="gpt-3.5-turbo-16k")
 
         # Use the general prompt template for the current section
-        prompt_template = load_section_prompt(lesson.current_section, getattr(lesson, lesson.current_section))
+        prompt_template = load_section_prompt(current_lesson.active_section, getattr(current_lesson, current_lesson.active_section))
 
         chain = LLMChain(prompt=prompt_template, llm=model)
 
         response = chain(
-            {"input": prompt, "chat_history": st.session_state.messages[-20:]},
+            {"input": user_input, "chat_history": st.session_state.messages[-20:]},
             include_run_info=True,
-            tags=[lesson.filename, lesson_type]
+            tags=[current_lesson.filename, selected_lesson_type]
         )
-        st.session_state.messages.append(HumanMessage(content=prompt))
+        st.session_state.messages.append(HumanMessage(content=user_input))
         st.session_state.messages.append(AIMessage(content=response[chain.output_key]))
         run_id = response["__run"].run_id
 
@@ -110,15 +110,15 @@ def display_feedback_buttons(run_id):
         st.button("👎", on_click=send_feedback, args=(run_id, 0))
 
 def initialize_state():
-    if st.session_state.get("current_lesson") != lesson_file or st.session_state.get("current_lesson_type") != lesson_type:
-        st.session_state["current_lesson"] = lesson_file
-        st.session_state["current_lesson_type"] = lesson_type
-        welcome_message = f"Once you are prepared, we shall begin our exploration of {lesson_file}. I will be your guide throughout this intellectual journey."
+    if st.session_state.get("current_lesson") != selected_lesson_file or st.session_state.get("current_lesson_type") != selected_lesson_type:
+        st.session_state["current_lesson"] = selected_lesson_file
+        st.session_state["current_lesson_type"] = selected_lesson_type
+        welcome_message = f"Once you are prepared, we shall begin our exploration of {selected_lesson_file}. I will be your guide throughout this intellectual journey."
         st.session_state["messages"] = [AIMessage(content=welcome_message)]
 
 # Message handling and interaction
 def send_feedback(run_id, score):
-    client.create_feedback(run_id, "user_score", score=score)
+    langsmith_client.create_feedback(run_id, "user_score", score=score)
 
 def display_messages():
     for msg in st.session_state["messages"]:
@@ -127,27 +127,27 @@ def display_messages():
         else:
             st.chat_message("assistant").write(msg.content)
 
-# Initialize LangSmith client
-client = Client()
+# Initialize LangSmith langsmith_client
+langsmith_client = Client()
 
-# Get all lesson files in the lessons directory
+# Get all current_lesson files in the lessons directory
 lesson_files = os.listdir("lessons")
 
 # Lesson selection sidebar
-lesson_file = st.sidebar.selectbox("Select Lesson", os.listdir("lessons"))
+selected_lesson_file = st.sidebar.selectbox("Select Lesson", os.listdir("lessons"))
 
 # Create a new Lesson object
-lesson = Lesson(lesson_file)
+current_lesson = Lesson(selected_lesson_file)
 # Display the title, background and prerequisites, and learning objectives
-lesson.display()
+current_lesson.display()
 
-# Radio buttons for lesson type selection
-lesson_type = st.sidebar.radio("Select Lesson Type", ["Instructions based lesson", "Interactive lesson with questions"])
+# Radio buttons for current_lesson type selection
+selected_lesson_type = st.sidebar.radio("Select Lesson Type", ["Instructions based lesson", "Interactive lesson with questions"])
 
 initialize_state()
 
 display_messages()
 
 # Handle user input and assistant responses
-if prompt := handle_user_input():
-    handle_assistant_response(prompt, lesson)
+if user_input := handle_user_input():
+    handle_assistant_response(user_input, current_lesson)
