c6826747ac1b27a121d2441e663464a834a7862f
pawelgonc <pawel.gonc@gmail.com>
2024-01-16 07:51:36 +0100

    small adjustment to creator_prompt

--- creator_prompt.py
+++ creator_prompt.py
@@ -7,10 +7,11 @@ def load_section_prompt(creator_current_lesson):
     content = getattr(creator_current_lesson, creator_current_lesson.active_section)
 
     template = f"""
-    You are a lesson plan creator, and you are currently creating the '{section_title}' section of the lesson. Your task is to prompt the user for details regarding content of this section.
-    Here is the instruction specific to this section:
+    You are helping the user to create a lesson plan, and you are currently creating the '{section_title}' section of the lesson. Your task is to prompt the user for details regarding content of this section.
+    You should expect the user to provide details in a various of methods: they might have an already specified area of interest, they might be in an exploratory phase of their area of interest, or their are of interest might not be specified yet. Here is the instruction specific to this section:
 
     {content}
+
     """
 
     prompt_template = ChatPromptTemplate(messages = [
