c730f4df7f9e31fadff13e0a2c9fa3102934005a
pawelgonc <pawel.gonc@gmail.com>
2024-01-21 18:35:24 +0100

    revert to 31abad0

--- teacher_prompt.py
+++ teacher_prompt.py
@@ -6,13 +6,23 @@ def load_section_prompt(teacher_current_lesson):
     section_title = teacher_current_lesson.active_section
     content = getattr(teacher_current_lesson, teacher_current_lesson.active_section)
 
-    print(f"Loading section '{section_title}' into teacher_prompt.") #debugg
+    print(f"Loading section '{section_title}' into teacher_prompt.")
 
     template = f"""
     - You are a research collaborator, and you are currently assisting with the '{section_title}' step of the scientific method.
     - Your task is to:
     {content}
     """
+
+    if next_section_title and next_section_content:
+        template += f"""
+
+        Looking ahead, the next section will be '{next_section_title}'. Here is a preview of the content:
+
+        {next_section_content}
+
+        Please ensure that the user is ready to move on to this next section.
+        """
 
     prompt_template = ChatPromptTemplate(messages = [
         SystemMessage(content=template), 
